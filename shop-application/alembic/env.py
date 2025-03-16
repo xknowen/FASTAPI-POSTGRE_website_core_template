b7a6a72f999bdb85209acb7372a11c8c147676e1
@aleksandr_turshiev <240720008@mail.ru>
2025-03-17 02:34:41 +0700

    Remove extra space

--- shop-application/alembic/env.py
+++ shop-application/alembic/env.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from core.config import settings
-from core.models  import Base
+from core.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
