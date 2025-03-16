6d9d99b2fb4ffecdc5d622852bc427479100db19
@aleksandr_turshiev <240720008@mail.ru>
2025-03-17 03:50:05 +0700

    add config for alembic users table migration

--- shop-application/core/config.py
+++ shop-application/core/config.py
@@ -19,6 +19,14 @@ class DatabaseConfig(BaseModel):
     pool_size: int = 50
     max_overflow: int = 10
 
+    naming_conventions: dict[str, str] = {
+        'ix': 'ix_%(column_0_name)s',
+        'uq': 'uq%(table_name)s_%(column_0_N_name)s',
+        'ck': 'ck_%(table_name)s_%(constraint_name)s',
+        'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+        'pk': 'pk_%(table_name)s'
+    }
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
