fe6459125debcade67cfdf7163ca5a215f187109
@aleksandr_turshiev <240720008@mail.ru>
2025-03-17 02:33:57 +0700

    Remove example lines

--- shop-application/utils/case_converter.py
+++ shop-application/utils/case_converter.py
@@ -1,12 +1,4 @@
 def camel_case_to_snake_case(input_str: str) -> str:
-    '''
-    >>> camel_case_to_snake_case('SomeSDK')
-    "some_sdk"
-    >>> camel_case_to_snake_case('RServoDrive')
-    "r_servo_drive"
-    >>> camel_case_to_snake_case('SDKDemo')
-    "sdk_demo"
-    '''
     chars = []
     for c_index, char in enumerate(input_str):
         if c_index and char.isupper():
